0ac29957ef9964257cb75afb42bc2140e9d4bfa4
isabella-conti <ircontif@gmail.com>
2023-11-17 17:53:11 -0300

    separar arquivos e renomear index

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -15,7 +15,7 @@ class SeriesController extends Controller
             'Gambito da Rainha'
         ];
 
-        return view('listar-series')->with('series',$series);
+        return view('series.index')->with('series',$series);
     }
 }
 
