dbcfa41b7f42b4ff7e3ce19c6eb6aad619a16f15
isabella-conti <ircontif@gmail.com>
2023-11-17 19:37:43 -0300

    estrutura layout

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Series</title>
-</head>
-<body>
-    <h1>Series</h1>
+<x-layout title="Séries">
     
 <ul>
     @foreach ($series as $serie)
@@ -14,5 +6,4 @@
     @endforeach
 
 </ul>    
-</body>
-</html>
+</x-layout>
